86ac673f71bda6e7207b5d2439045ffa9278f35a
William Bryant <gogobebe2@gmail.com>
2021-03-01 21:53:59 +1300

    170

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
